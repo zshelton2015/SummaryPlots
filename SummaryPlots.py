5b140574d05e5102f06f63579b202a808aa1ef36
Zach's Temp Account <zachstempaccount@noahs-air.dhcp.fnal.gov>
2018-06-13 14:51:47 -0500

    Adding Test Program

--- SummaryPlots.py
+++ SummaryPlots.py
@@ -7,37 +7,44 @@ import sqlite3
 from ROOT import *
 import pprint
 def SummaryPlot(database):
-
+bins = [0,1,2,3]
+shunts = [1,1.5,2,3,4,5,6,7,8,9,10,11,11.5]
     xyz1234 = sqlite3.connect(database)
     cursor = xyz1234.cursor()
+    fcard = []
+    frange= []
+    fshunt= []
+    FailedCards = {'Card':fcard,'Ranges':frange,'Shunts':fshunt}
     name = database[24:47]
     #Modify rootout change title of output ROOT file
     rootout = name
     rootout = TFile("%s.root"%name,"recreate")
-    for x in bins:
-        for k in shunts:
+    for r in bins:
+        for sh in shunts:
             #Fetch the values of slope and offset for the corresponding shunt and range
-            values = cursor.execute("select slope,offset from qieshuntparams where range=%i and shunt=%.1f;"%(x,k)).fetchall()
+            values = cursor.execute("select slope,offset from qieshuntparams where range=%i and shunt=%.1f;"%(sh,r)).fetchall()
             #Fetch Max and minimum values
-            maxmin = cursor.execute("select max(slope),min(slope) from qieshuntparams where range=%i and shunt = %.1f;"%(x,k)).fetchall()
+            maxmin = cursor.execute("select max(slope),min(slope) from qieshuntparams where range=%i and shunt = %.1f;"%(sh,r)).fetchall()
             #SQLITE3 values are tuples, this turns the tuple into 2 numbers that can be used for ROOT arguments
             maximum , minimum = maxmin[0]
-            maximums= maximum+.1/k
-            minimums= minimum-.1/k
-            #Make a Canvas and histogram for the shunts that's added to the list
-            c.append(TCanvas("%s Shunt%.1f V -  Range %i"%(name,k,x),"histo"))
-            histshunt.append(TH1D("%s SLOPE Shunt %.1f - Range %i"%(name,k,x), "%s Shunt %.1f - Range %i"%(name,k,x),100,minimums,maximums))
-            c.GetXaxis().SetTitle("Slope")
-            c.GetYaxis().SetTitle("Frequency")
-            maxmin = cursor.execute("select max(offset),min(offset) from qieshuntparams where range=%i and shunt = %.1f;"%(x,k)).fetchall()
+            maximums= maximum+.1/sh
+            minimums= minimum-.1/sh
+            #Mashe a Canvas and histogram for the shunts that's added to the list
+            c.append(TCanvas("%s SLOPE Shunt %.1f  -  Range %i"%(name,sh,r),"histo"))
+            histshunt.append(TH1D("%s SLOPE - Shunt %.1f - Range %i"%(name,sh,r), "%s Shunt %.1f - Range %i"%(name,sh,r),100,minimums,maximums))
+            c.SetXTitle("Slope")
+            c.GetYTitle("Frequency")
+            c.SetLogY()
+            maxmin = cursor.execute("select max(offset),min(offset) from qieshuntparams where range=%i and shunt = %.1f;"%(r,sh)).fetchall()
             maximumo, minimumo = maxmin[0]
-            maximumo+=k*5
-            minimumo-=k*5
+            maximumo+=sh*5
+            minimumo-=sh*5
             #Make a Canvas and histogram for the offset that's added to the list
-            c2.append(TCanvas("%s OFFSET  %.1f V Range %i"%(name,k,x) ,"histo"))
-            histoffset.append(TH1D("%s OFFSET Shunt %.1f - Range %d"%(name,k,x), "%s Shunt %.1f - Range %d"%(name,k,x),50,minimumo,maximumo))
-            c2.GetXaxis().SetTitle("Offset")
-            c2.GetYaxis().SetTitle("Frequency")
+            c2.append(TCanvas("%s OFFSET Shunt %.1f Range %i"%(name,sh,r) ,"histo"))
+            histoffset.append(TH1D("%s OFFSET  Shunt %.1f - Range %d"%(name,sh,r), "%s Shunt %.1f - Range %d"%(name,sh,r),50,minimumo,maximumo))
+            c2.SetXTitle("Offset")
+            c2.GetYTitle("Frequency")
+            c2.SetLogY()
             #Fills the histograms with the values fetched above
             for val in values:
                 slope , offset = val
@@ -48,42 +55,43 @@ def SummaryPlot(database):
             histoffset[-1].Write()
             if sh==1:
                 if 0.283>minimum or maximum>0.326:
-                    print "%s, %i, 1, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
+                    F
             if sh==1.5:
                 if 0.185>minimum or maximum>0.22:
-                    print "%s, %i, 1.5, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
             if sh==2:
                 if 0.143>minimum or maximum>0.168:
-                    print "%s, %i, 2, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
             if sh==3:
                 if 0.095>minimum or maximum>0.115:
-                    print "%s, %i, 3, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
             if sh==4:
                 if 0.072>minimum or maximum>0.085:
-                    print "%s, %i, 4, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
             if sh==5:
                 if 0.0575>minimum or maximum>0.068:
-                    print "%s, %i, 5, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
             if sh==6:
                 if 0.048>minimum or maximum>0.064:
-                    print "%s, %i, 6, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
             if sh==7:
                 if 0.041>minimum or maximum>0.05:
-                    print "%s, %i, 7, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
             if sh==8:
                 if 0.036>minimum or maximum>0.044:
-                    print "%s, %i, 8, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
             if sh==9:
                 if 0.032>minimum or maximum>0.039:
-                    print "%s, %i, 9, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
             if sh==10:
                 if 0.029>minimum or maximum>0.035:
-                    print "%s, %i, 10, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
             if sh==11:
                 if 0.026>minimum or maximum>0.032:
-                    print "%s, %i, 11, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
             if sh==11.5:
                 if 0.025>minimum or maximum>0.031:
-                    print "%s, %i, 11.5, Bad, %.4f, %.4f"%(cardname,r,minimum,maximum)
+                    print "Card %s in Shunt %.1f in Range %i failed"%(name,sh,r)
     rootout.Close()
 return "Summary Plots Made!" ,
---
+++ test.py
@@ -0,0 +1,3 @@
+import SummaryPlots.py
+
+SummaryPlots("qieCalibrationParameters_0xa6000000_0xeabacd70.db")
