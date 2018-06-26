27e05faccc85fbefae8afb5e6bdefe2ff21327ef
Zach's Temp Account <zachstempaccount@noahs-air.dhcp.fnal.gov>
2018-06-26 16:08:28 -0500

    Added unique name

--- SummaryPlot.py
+++ SummaryPlot.py
@@ -72,7 +72,7 @@ def SummaryPlot(options):
         for f in options.dbnames:
             files.append(glob.glob("data/%s/Run_%s/%s"%(date,run,f))[0])
             print files
-    MergeDatabases(files, "data/%s/Run_%s/"%(date, run))
+    MergeDatabases(files, "data/%s/Run_%s/"%(date, run),"MergedDatabaseRun%s.db"%run)
     xyz1234 = sqlite3.connect("data/%s/Run_%s/mergedDatabase.db"%(date, run))
     cursor = xyz1234.cursor()
     TGaxis.SetMaxDigits(3)
